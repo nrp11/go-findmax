3fe7b3024e7b39b803555fafbdcd0ff7543448c0
nrp11 <nrpatil1988@gmail.com>
2017-03-05 11:38:10 -0800

    enhanced my find max program

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("my nam feature branch first commit")
+	fmt.Println("Hey we are going to find max in the below array")
 	a := []int{5, 1, 4, 8, 6, 3}
+	for _, v := range a {
+		fmt.Println(v)
+	}
+
 	max := a[0]
 	for i := 1; i < len(a); i++ {
 		if a[i] > max {
