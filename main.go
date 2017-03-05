71f19c7725db9f807f149d0a02ad9ac033819197
nrp11 <nrpatil1988@gmail.com>
2017-03-05 11:26:26 -0800

    Find max from given array.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{5, 1, 4, 8, 6, 3}
+	max := a[0]
+	for i := 1; i < len(a); i++ {
+		if a[i] > max {
+			max = a[i]
+		}
+	}
+	fmt.Println("Max:", max)
+}
